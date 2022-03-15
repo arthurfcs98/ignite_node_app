7ddabef2511e833978cb95f46bdb3847dc5c9583
Arthur Freitas <cesarinoarthur@gmail.com>
2022-03-15 14:39:04 -0300

    specifications

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,3 +1,5 @@
+import { SpecificationRepository } from "../repositories/SpecificationRepository";
+
 interface IRequest {
     name: string;
     description: string;
@@ -5,7 +7,7 @@ interface IRequest {
 
 class CreateSpecificationService {
     // eslint-disable-next-line prettier/prettier
-    constructor(private specificationRepository) { }
+    constructor(private specificationRepository: SpecificationRepository) { }
 
     execute({ name, description }: IRequest): void {
         const specificationAlreadyExists =
