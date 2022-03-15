3a2f8b7d97abfea021609d49b2b744b75fdd924b
Arthur Freitas <cesarinoarthur@gmail.com>
2022-03-15 12:02:26 -0300

    adicionando modules

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,22 @@
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService {
+    // eslint-disable-next-line prettier/prettier
+    constructor(private specificationRepository) { }
+
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationRepository.create({ name, description });
+    }
+}
+
+export { CreateSpecificationService };
