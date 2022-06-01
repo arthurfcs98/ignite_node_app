b8a43a4d3524cf2b57f2c57f3e36d14581b31e99
Arthur Cesarino <cesarinoarthur@gmail.com>
2022-06-01 15:47:20 -0300

    remove unecessary username on table users

---
+++ src/database/migrations/1654108533723-AlterTableUsersRemoveUsername.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AlterTableUsersRemoveUsername1654108533723
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("users", "username");
+        await queryRunner.changeColumn(
+            "users",
+            "email",
+            new TableColumn({
+                name: "email",
+                type: "varchar",
+                isUnique: true,
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.changeColumn(
+            "users",
+            "email",
+            new TableColumn({
+                name: "email",
+                type: "varchar",
+            })
+        );
+        await queryRunner.addColumn(
+            "users",
+            new TableColumn({
+                name: "username",
+                type: "varchar",
+                isUnique: true,
+            })
+        );
+    }
+}
