2951a36066aeded1f5d9dc4e3783b2e3b5738674
Arthur Freitas Cesarino dos Santos <cesarinoArthur@gmail.com>
2022-07-22 08:07:53 -0300

    forgot password flow

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,14 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
+import { EtherealMailProvider } from "./MailProvider/EtherealMailProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
     DayjsDateProvider
+);
+container.registerInstance<IMailProvider>(
+    "EtherealMailProvider",
+    new EtherealMailProvider()
 );
--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -2,12 +2,20 @@ import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
+import { SendForgotPasswordEmailController } from "@modules/accounts/useCases/sendForgotPasswordEmail/SendForgotPasswordEmailController";
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 const refreshTokenController = new RefreshTokenController();
+const sendForgotPasswordEmailController =
+    new SendForgotPasswordEmailController();
 
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
 authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
+authenticateRoutes.post(
+    "/password/forgot",
+    sendForgotPasswordEmailController.handle
+);
+// authenticateRoutes.post("/password/reset", refreshTokenController.handle);
 export { authenticateRoutes };
