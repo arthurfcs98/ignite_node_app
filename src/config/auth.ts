021622c2f7a12088775c7913f905a5a11ccb627e
Arthur Freitas Cesarino dos Santos <cesarinoArthur@gmail.com>
2022-07-22 05:29:46 -0300

    start refresh token flow

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export const auth = {
+    secret_token: "tokenKey",
+    expires_in_token: "1m",
+    expires_in_refresh_token: "30d",
+    secret_refresh: "refreshKey",
+    expires_refresh_token_days: 30,
+};
