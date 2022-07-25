4e602dc5de5c8645e8aaa14c3295f2b08b425c89
Arthur Freitas Cesarino dos Santos <cesarinoArthur@gmail.com>
2022-07-25 19:29:25 -0300

    start aws s3 with storage provider pt1

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export const auth = {
     secret_token: "tokenKey",
-    expires_in_token: "1m",
+    expires_in_token: "30d",
     expires_in_refresh_token: "30d",
     secret_refresh: "refreshKey",
     expires_refresh_token_days: 30,
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,11 +4,19 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
     DayjsDateProvider
 );
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    LocalStorageProvider
+);
+
 container.registerInstance<IMailProvider>(
     "EtherealMailProvider",
     new EtherealMailProvider()
